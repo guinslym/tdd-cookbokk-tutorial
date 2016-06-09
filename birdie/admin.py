0c9e15cb2962b7b8b95f6c16b77db5b84f57bf74
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-09 00:33:41 -0400

    testing the views and the Admin part 2

--- birdie/admin.py
+++ birdie/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('exerpt', )
+
+    def excerpt(self, obj):
+        return obj.get_excerpt(5)
+
+admin.site.register(Post, PostAdmin)
--- birdie/tests/test_admin.py
+++ birdie/tests/test_admin.py
@@ -11,8 +11,8 @@ pytestmark = pytest.mark.django_db
 class TestPostAdmin:
     def test_excerpt(self):
         site = AdminSite()
-        post_admin = admin.PostAdmin(models.Post, Site)
+        post_admin = admin.PostAdmin(models.Post, site)
 
         obj = mixer.blend('birdie.Post', body='Hello')
-        result = post_admin.get_excerpt(obj)
+        result = post_admin.excerpt(obj)
         assert result == 'Hello', 'Should return first few characters'
