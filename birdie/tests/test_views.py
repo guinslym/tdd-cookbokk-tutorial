b8a74ee61e4abb0c6a9d7c20983be80c30800829
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-09 00:49:47 -0400

    adding login required test

--- birdie/tests/test_views.py
+++ birdie/tests/test_views.py
@@ -1,4 +1,9 @@
+import pytest
 from django.test import RequestFactory
+from django.contrib.auth.models import AnonymousUser
+from mixer.backend.django import mixer
+
+pytestmark = pytest.mark.django_db
 
 from .. import views
 
@@ -7,3 +12,17 @@ class TestHomeView:
         req = RequestFactory().get('/')
         resp = views.HomeView.as_view()(req)
         assert resp.status_code == 200, 'Should be callable by anyone'
+
+class TestAdminView:
+    def test_anonymous(self):
+        req = RequestFactory().get('/')
+        req.user = AnonymousUser()
+        res = views.AdminView.as_view()(req)
+        assert 'login' in res.url
+
+    def test_superuser(self):
+        user = mixer.blend('auth.User', is_superuser=True)
+        req = RequestFactory().get('/')
+        req.user = user
+        resp = views.AdminView.as_view()(req)
+        assert  resp.status_code == 200, 'Authenticated user can access'
